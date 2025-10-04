a3fbdc5418c2a1f1989b1ac5a0da7ddd196bcfb7
junho@bobsweep.com <shinjuno123@naver.com>
2025-10-03 18:38:57 -0700

    End of section 5

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject, signal } from '@angular/core';
-import { User } from '../../types/user';
+import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { tap } from 'rxjs';
 
 @Injectable({
@@ -12,12 +12,21 @@ export class AccountService {
 
   baseUrl = 'https://localhost:5001/api/';
 
-  login(creds: any) {
+  register(creds: RegisterCreds) {
+    return this.http.post<User>(this.baseUrl + 'account/register', creds).pipe(
+      tap(user => {
+        if(user) {
+          this.setCurrentUser(user);
+        }
+      })
+    );
+  }
+
+  login(creds: LoginCreds) {
     return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
       tap(user => {
         if(user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUser.set(user);
+          this.setCurrentUser(user);
         }
       })
     );
@@ -26,5 +35,10 @@ export class AccountService {
   logout() {
     localStorage.removeItem('user');
     this.currentUser.set(null);
+  }
+
+  setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUser.set(user);
   }
 }
